ef8f4910a36231570505fcc7ca6ab8e9acbf7e0d
AbirBendouda <64878997+AbirBen99@users.noreply.github.com>
2023-01-18 00:02:16 +0100

    Add files via upload

---
+++ OrdreModal.jsx
@@ -0,0 +1,92 @@
+import {
+    Dialog, Grid, TextField
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+// import { withStyles } from "@mui/styles";
+import { useEffect, useState } from "react";
+
+
+import { changeOrdre, createMission } from "../Api/Mission";
+
+
+// const styles = {
+//   dialogPaper: {
+//     minHeight: "45vh",
+//     maxHeight: "45vh",
+//   },
+// };
+
+function BasicModal({ handleClose, open,id }) {
+
+  const [formValues, setFormValues] = useState({ ordre:""});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues(() => {
+      return {
+        [name]: value,
+      };
+    });
+    console.log(formValues);
+  };
+  const handleSubmit = () => {
+    //api call
+    changeOrdre(id,formValues.ordre);
+  };
+
+  return (
+    <div>
+      <Dialog
+        fullWidth="true"
+        maxWidth="md"
+        maxHeight="md"
+        open={open}
+        onClose={handleClose}
+        scroll="body"
+      
+      
+      >
+        <form onSubmit={() => handleSubmit()}>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12}>
+              <Box
+                sx={{ color: "#337f94" }}
+                className="fw-bold text-center mt-2"
+              >
+                Changer l'ordre
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-basic"
+                label="ordre"
+                variant="outlined"
+                fullWidth
+                name="ordre"
+                value={formValues.ordre}
+                onChange={handleChange}
+              />
+             
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button sx={{ backgroundColor:"#337f94", color:"white" }}
+              variant="outlined" type="submit">
+                Modifier
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Dialog>
+    </div>
+  );
+}
+
+// export default withStyles(styles)(BasicModal);
+
+export default BasicModal;
